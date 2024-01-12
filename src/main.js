ee6b4f1d9bda7652a38953c6bc5ed0e35d7e012c
redtom213 <ktbk007@naver.com>
2024-01-13 04:27:59 +0900

    local code reset

--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
 import '/assets/main.css'
 
 import {createApp} from "vue";
-import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
-import {SwiperSlide} from "swiper/vue";
+import { Swiper, SwiperSlide } from 'swiper/vue';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 import App from './App.vue'
-
 
 const app = createApp(App);
 
